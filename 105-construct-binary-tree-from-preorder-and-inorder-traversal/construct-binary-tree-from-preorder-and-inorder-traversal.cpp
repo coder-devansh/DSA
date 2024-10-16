1323fefb527de5572a6cea0cf0a6c79965e410aa
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2024-10-16 06:01:08 +0530

    Time: 7 ms (92.71%) | Memory: 26.7 MB (93.98%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* construct(vector<int>& preorder, int prestart, int preend, 
+                        vector<int>& inorder, int instart, int inend) {
+        if (prestart > preend || instart > inend) return nullptr;
+
+        TreeNode* root = new TreeNode(preorder[prestart]);
+        int inrootIndex = instart; // Index of the root in inorder array
+        while (inorder[inrootIndex] != root->val) {
+            inrootIndex++;
+        }
+        
+        int numsLeft = inrootIndex - instart; // Number of nodes in left subtree
+
+        root->left = construct(preorder, prestart + 1, prestart + numsLeft, 
+                                inorder, instart, inrootIndex - 1);
+        root->right = construct(preorder, prestart + numsLeft + 1, preend, 
+                                 inorder, inrootIndex + 1, inend);
+        
+        return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return construct(preorder, 0, preorder.size() - 1, 
+                         inorder, 0, inorder.size() - 1);
+    }
+};

934d6efa9cf519dc6a276d0db8a8c0ef1847833b
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2024-10-20 09:55:08 +0530

    Time: 47 ms (100.00%) | Memory: 111.6 MB (25.00%) - LeetSync

---
+++ 3566-find-the-sequence-of-strings-appeared-on-the-screen/find-the-sequence-of-strings-appeared-on-the-screen.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+void find(string target){
+
+}
+    vector<string> stringSequence(string target) {
+        string ans="a";
+         vector<string>result;
+
+        while(ans!=target){
+            if(ans==target.substr(0,ans.size()))
+            {
+                result.push_back(ans);
+                ans+='a';
+
+            }else{
+                result.push_back(ans);
+                ans[ans.size()-1]=ans[ans.size()-1]+1;
+                
+            }
+
+        }
+        result.push_back(target);
+        return result;
+                     
+        
+    }
+};

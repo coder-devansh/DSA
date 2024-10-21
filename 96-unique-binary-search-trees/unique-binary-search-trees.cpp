9bb5d2cc319617407f4549d397e714c43f101b88
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2024-10-21 23:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (47.60%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+double  fact(int n){
+    double fact=1;
+    for(int i=2;i<=n;i++){
+        fact*=i;
+    }
+    return fact;
+}
+    int numTrees(int n) {
+        if(n>=19 || n==15 || n==14){
+              return (fact(2*n)/(fact(n+1)*fact(n)))+1;
+
+        }
+        return (fact(2*n)/(fact(n+1)*fact(n)));
+        
+    }
+};

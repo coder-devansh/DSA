63f4a6ae82cc4d90bf1ff2c82742cd804f5008de
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2024-11-16 14:26:10 +0530

    Time: 5 ms (16.03%) | Memory: 45.5 MB (27.66%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
@@ -19,7 +19,7 @@ class Solution {
         for(int i=0;i<=nums.length-k;i++){
             r=i;
             int index=r+k;
-            int count=0;
+           
             
             int maxi=0;
             int found=0;

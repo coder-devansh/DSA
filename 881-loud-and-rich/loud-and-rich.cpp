77ba6566ba995491ee2146105b6369531a984c36
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2024-11-18 01:19:10 +0530

    Time: 12 ms (54.73%) | Memory: 50.7 MB (49.45%) - LeetSync

--- 881-loud-and-rich/loud-and-rich.cpp
+++ 881-loud-and-rich/loud-and-rich.cpp
@@ -1,46 +1,45 @@
 class Solution {
 public:
-    // Function to perform DFS
-    void dfs(int node, vector<int>& quiet, vector<int>& ans, vector<vector<int>>& adj) {
-        // If the answer for the current node is already computed, no need to process it again
-        if (ans[node] != -1) return;
-        
-        // Initially, the answer for this node is the node itself
-        ans[node] = node;
-        
-        // Traverse all the richer people of the current node
-        for (int neighbor : adj[node]) {
-            // Recursively process the neighbor
-            dfs(neighbor, quiet, ans, adj);
-            
-            // If the neighbor has a quieter person, update the answer for the current node
-            if (quiet[ans[neighbor]] < quiet[ans[node]]) {
-                ans[node] = ans[neighbor];
-            }
+void dfs(int node,vector<int>adj[],vector<int>&quiet,vector<int>&ans){
+    if(ans[node]!=-1)
+    {
+        return;
+    }
+    ans[node]=node;
+    
+
+    for(auto it:adj[node])
+    {
+        dfs(it,adj,quiet,ans);
+        if(quiet[ans[it]]<quiet[ans[node]])
+        {
+            ans[node]=ans[it];
         }
     }
     
+}
     vector<int> loudAndRich(vector<vector<int>>& richer, vector<int>& quiet) {
-        int n = quiet.size();
-        
-        // Adjacency list to store the graph
-        vector<vector<int>> adj(n);
-        
-        // Building the adjacency list from the richer relations
-        for (const vector<int>& edge : richer) {
-            adj[edge[1]].push_back(edge[0]);
+        int n=quiet.size();
+        vector<int>adj[n];
+        for(auto it:richer){
+            adj[it[1]].push_back(it[0]);
         }
-        
-        // Result array to store the answer for each person
-        vector<int> ans(n, -1);
-        
-        // Perform DFS for each person if their answer is not computed
-        for (int i = 0; i < n; ++i) {
-            if (ans[i] == -1) {
-                dfs(i, quiet, ans, adj);
+     
+        vector<int>ans(n,-1);
+
+        for(int i=0;i<quiet.size();i++){
+            if(ans[i]==-1)
+            {
+                 ( dfs(i,adj,quiet,ans));
+
             }
         }
+           
+          
         
+          
+           
         return ans;
+        
     }
 };

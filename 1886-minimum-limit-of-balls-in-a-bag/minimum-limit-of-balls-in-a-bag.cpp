8c50ad608a588db9d15bc793dea950127947490f
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2024-12-07 21:42:34 +0530

    Time: 43 ms (35.11%) | Memory: 122 MB (5.43%) - LeetSync

---
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+bool canwe(int mid,vector<int>nums,int k){
+    int count=0;
+     for(auto it:nums){
+        count+=(it-1)/mid;
+
+     }
+     return count<=k;
+
+}
+    int minimumSize(vector<int>& nums, int k) {
+        
+        int maxi=0;
+        for(auto it:nums){
+            maxi=max(maxi,it);
+        }
+        int low=1;
+        int high=maxi;
+        while(low<high){
+            int mid=(low+high)/2;
+            if(canwe(mid,nums,k)==true){
+                high=mid;
+            }else{
+                low=mid+1;
+            }
+        }
+        return high;
+        
+    }
+};

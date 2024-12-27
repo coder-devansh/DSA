b6b22f49d091779ac64e246302f4b065835aaad1
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2024-12-27 23:49:26 +0530

    Time: 4 ms (23.39%) | Memory: 47.3 MB (24.68%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -3,15 +3,14 @@ public:
     int maxScoreSightseeingPair(vector<int>& values) {
         vector<int>vec(values.size(),0);
         int maxi=0;
-        for(int i=0;i<values.size();i++){
-            maxi=max(maxi,values[i]+i);
-            vec[i]=maxi;
-        }
+        // for(int i=0;i<values.size();i++){
+        //     maxi=max(maxi,values[i]+i);
+        //     vec[i]=maxi;
+        // }
         int maxi1=0;
         for(int j=1;j<values.size();j++){
-            maxi1=max(maxi1,values[j]-j+vec[j-1]);
-
-
+            maxi=max(maxi,values[j-1]+j-1);
+            maxi1=max(maxi1,values[j]-j+maxi);
         }
         return maxi1;
         

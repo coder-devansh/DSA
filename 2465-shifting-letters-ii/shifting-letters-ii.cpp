d2ab3694242b386a8e9c33a56bcb65375b3b278b
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-01-06 00:23:58 +0530

    Time: 358 ms (7.60%) | Memory: 347.5 MB (5.07%) - LeetSync

---
+++ 2465-shifting-letters-ii/shifting-letters-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        vector<int>prefix(5000001,0);
+        for(auto it:shifts){
+            int start=it[0];
+            int end=it[1];
+            int dir=it[2];
+            if(dir==0){
+                dir=-1;
+            }
+            prefix[start]+=dir;
+            if(end+1<s.size()){
+                prefix[end+1]-=dir;
+            }
+           
+            }
+            for(int i=1;i<s.size();i++){
+                prefix[i]+=prefix[i-1];
+               
+            }
+            int n=s.size();
+             for (int i = 0; i < n; i++) {
+        int shift = prefix[i];
+        int newChar = (s[i] - 'a' + shift) % 26; 
+        if (newChar < 0) {
+            newChar += 26;
+        }
+        s[i] = 'a' + newChar; 
+    }
+
+            
+           
+        
+        return s;
+        
+    }
+};

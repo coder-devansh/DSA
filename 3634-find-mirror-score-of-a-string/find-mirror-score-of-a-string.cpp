3f6bf86f917683c3fbe07b2b6824a39c1ea66bab
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-01-05 13:40:39 +0530

    Time: 35 ms (33.33%) | Memory: 23.5 MB (50.00%) - LeetSync

--- 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.cpp
+++ 3634-find-mirror-score-of-a-string/find-mirror-score-of-a-string.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-// a==z 
-// mirror=('a'+('z'-a))
-// mirror=z;
-// "aczzx"
-// mirror='a'+('z'-ch)
-
 
     long long calculateScore(string s) {
-        unordered_map<char,stack<int>>mp;
-        vector<bool>used(s.size()+1,false);
         long long score=0;
+        vector<bool>used(s.size(),false);
+        unordered_map<char,stack<int>>mp;
         for(int i=0;i<s.size();i++){
             char ch=s[i];
             char mirror='a'+('z'-ch);
             if(!mp[mirror].empty() && mp.find(mirror)!=mp.end()){
                 int j=mp[mirror].top();
-                
-                if(!used[j] &&  j<i){
+                if(!used[j] && j<i){
                     score+=i-j;
-                    used[j]=true;
                     used[i]=true;
+                    used[j]=true;
                     mp[mirror].pop();
-
                 }
-            }if(!used[i]){
-                 mp[s[i]].push(i);
-
             }
-               
-            
-           
-
+            if(!used[i])
+            {
+                mp[s[i]].push(i);
+            }
         }
         return score;
-
-    
         
     }
 };

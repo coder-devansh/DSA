5c989300906b2c8a0185eebe909f8136a3a8cebc
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-01-07 00:06:45 +0530

    Time: 95 ms (25.85%) | Memory: 12.2 MB (50.97%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,38 +1,19 @@
-   class Solution {
+class Solution {
 public:
     vector<int> minOperations(string boxes) {
         vector<int>ans(boxes.size(),0);
-
+        
         for(int i=0;i<boxes.size();i++){
             long long sum=0;
             for(int j=0;j<boxes.size();j++){
-                if(boxes[j]=='1')
-                {
+                if(boxes[j]-'0'==1){
                     sum+=abs(i-j);
                 }
+
             }
             ans[i]=sum;
-
         }
         return ans;
-        
-    //    vector<int>prefix(boxes.size(),0);
-    //    vector<int>suffix(boxes.size(),0);
-    //    long long sum=0;
-    //    for(int i=boxes.size()-1;i>=0;i--){
-    //     sum+=boxes[i]-'0';
-    //     prefix[i]=sum;
-    //    }
-    //    sum=0;
-    //    vector<int>ans(boxes.size(),0);
-    //    for(int i=0;i<boxes.size();i++){
-    //     sum+=boxes[i]-'0';
-    //     suffix[i]=sum;
-    //    }
-    //    for(int i=0;i<boxes.size();i++){
-           
-    //    }
-    //    return ans;
         
     }
 };

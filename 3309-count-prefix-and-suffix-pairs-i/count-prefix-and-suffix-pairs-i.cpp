cfe728ec7d144a4315f5ba1ced8719767dc3687a
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-01-09 02:37:24 +0530

    Time: 8 ms (23.99%) | Memory: 23.7 MB (58.39%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
+// string word=ans.substr(0,,end);
     int countPrefixSuffixPairs(vector<string>& words) {
         long long count=0;
+
         for(int i=0;i<words.size();i++){
-            for(int j=0;j<words.size();j++){
+            for(int j=i+1;j<words.size();j++)
+            {
                 string word1=words[i];
                 string word2=words[j];
-                if(i<j && word1.size()<=word2.size())
-                {
+                if(word1.size()<=word2.size()){
                     string prefix=word2.substr(0,word1.size());
                     string suffix=word2.substr(word2.size()-word1.size());
-                    if(prefix==word1 && suffix==word1)
-                    {
-                        count++;
+                    if(prefix==word1 && suffix==word1){
+                        count+=1;
                     }
-                    cout<<prefix<<endl;
-                    cout<<suffix<<endl;
-                    
                 }
+
+
             }
         }
         return count;

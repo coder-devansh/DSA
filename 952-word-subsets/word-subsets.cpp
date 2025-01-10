86c8f5856b85047a091bc1923a03784e6ef26c5d
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-01-10 17:23:59 +0530

    Time: 1743 ms (5.02%) | Memory: 120.7 MB (28.31%) - LeetSync

---
+++ 952-word-subsets/word-subsets.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        map<char,int>mp;
+        for(auto it:words2){
+            map<char,int>temp;
+           string ans=it;
+          for(int i=0;i<ans.size();i++){
+            temp[ans[i]]++;
+           if(mp.find(ans[i])==mp.end()){
+            mp[ans[i]]++;
+           }
+           if(mp[ans[i]]<temp[ans[i]])
+           {
+            mp[ans[i]]=temp[ans[i]];
+           }
+
+            
+        }
+    }
+        vector<string>answer;
+        
+        for(int i=0;i<words1.size();i++){
+            map<char,int>element;
+            string ans=words1[i];
+            for(int j=0;j<ans.size();j++){
+              
+                if(mp.find(ans[j])!=mp.end() && mp[ans[j]]>element[ans[j]]){
+                    element[ans[j]]++;
+                }
+
+                
+            }
+            for(auto it:element){
+                cout<<it.first<<" "<<it.second<<endl;
+            }
+            if(mp==element){
+                answer.push_back(ans);
+            }
+        }
+        return answer;
+        
+    
+        
+    }
+};

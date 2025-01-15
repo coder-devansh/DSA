9c4821da28ac3059ae752451eee9ee33c87bd083
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-01-15 23:56:55 +0530

    Time: 35 ms (23.39%) | Memory: 46.2 MB (12.79%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -2,39 +2,46 @@
 
 class Solution {
 public:
-vector<vector<int>>dp;
 int mod=1e9+7;
-int find(int i,int amount,vector<int>&coins){
-    if(i==0){
-        return amount%coins[i]==0;
-    }
+// int find(int i,int amount,vector<int>&coins){
+//     if(i==0){
+//         return amount%coins[i]==0;
+//     }
    
-    if(dp[i][amount]!=-1){
-        return dp[i][amount];
-    }
-    int take=0;
-    if(amount>=coins[i]){
-          take=find(i,amount-coins[i],coins);
-    }
+//     if(dp[i][amount]!=-1){
+//         return dp[i][amount];
+//     }
+//     int take=0;
+//     if(amount>=coins[i]){
+//           take=find(i,amount-coins[i],coins);
+//     }
    
-    int nottaken=find(i-1,amount,coins);
-    return dp[i][amount]=(take+nottaken);
-}
+//     int nottaken=find(i-1,amount,coins);
+//     return dp[i][amount]=(take+nottaken);
+// }
     int change(int amount, vector<int>& coins) {
-        if(coins.size()==1 ){
-            if(amount % coins[0]==0){
-                return 1;
+        int n=coins.size();
+      vector<vector<double >>dp(n,vector<double >(amount+1,0));
+        for(int i=0;i<=amount;i++){
+            if((i % coins[0])==0){
+                dp[0][i]=1;
+            }
+            else{
+                dp[0][i]=0;
             }
         }
-        if(amount==0){
-            return 1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=amount;j++){
+                double  take=0;
+                if(j>=coins[i])
+                {
+                    take=dp[i][j-coins[i]];
+                }
+                    double  nottaken=dp[i-1][j];
+                dp[i][j]=(take+nottaken);
+            }
         }
-        dp.resize(coins.size(),vector<int>(50001,-1));
-
-        
-        int n=coins.size();
-       return  find(n-1,amount,coins);
-        
+        return dp[n-1][amount];
         
     }
 };

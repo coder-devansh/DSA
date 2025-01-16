67b37f6db034189ea4ae171fda7008299745db9e
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-01-16 15:15:48 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (40.55%) - LeetSync

--- 2509-minimize-xor/minimize-xor.cpp
+++ 2509-minimize-xor/minimize-xor.cpp
@@ -1,60 +1,45 @@
 class Solution {
 public:
-bool setbit(int result,int bit){
-    return result & (1<<bit);
+bool isSet(int result,int bit){
+    return result & 1<<bit;
 }
     int minimizeXor(int num1, int num2) {
-        int value;
-        value=num2;
-        int count=0;
+        int value=num2;
+        int targetbits=0;
+        int setbits=0;
+        while(value>0){
+            targetbits+=(1 & value);
+            value=value>>1;
+            
 
-        while(num2>0){
-             if((1 & num2)!=0){
-                count++;
-             }
-
-            num2=num2>>1;
-           
-    
         }
-        num2=value;
-        int count2=0;
-    long long  result=num1;
-    long long  temp=result;
-    while(temp>0){
-       if((temp & 1)!=0){
-        count2++;
-       }
-       temp=temp>>1;
-    }
-    cout<<count<<endl;
-    cout<<count2;
-    if(count2>count){
-        long long  currentbit=0;
-        while(count2>count){
-            if(setbit(result,currentbit)){
-                result=result & ~(1<<currentbit);
-                count2--;
-            }
-            currentbit++;
-
-    }
-    }
-    else if(count>count2){
+        int result=num1;
+        int temp=result;
+        while(temp>0){
+            setbits+=(1 & temp);
+            temp=temp>>1;
+        }
         int currentbit=0;
-        while(count>count2){
-            if(!setbit(result,currentbit)){
-                result= result | (1<<currentbit);
-                count2++;
+        while(setbits<targetbits){
+            if(!isSet(result,currentbit))
+            {
+                result=result | (1<<currentbit);
+                setbits++;
+
+
+
             }
             currentbit++;
-
-
         }
+        while(setbits>targetbits){
+            if(isSet(result,currentbit)){
+                result=result & ~(1<<currentbit);
+                setbits--;
+            }
+            currentbit++;
+        }
+        return result;
 
-    }
-    
-    return result;   
         
     }
 };

94572e331321a316bc66afc2b2e30c7a50f231e0
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-01-30 21:35:46 +0530

    Time: 6 ms (8.83%) | Memory: 22.1 MB (5.23%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,41 +1,44 @@
 class Solution {
 public:
-bool dfs(int node,vector<int>adj[],int col,vector<int>&vis){
-    vis[node]=col;
-    for(auto it:adj[node])
-    {
-        if(vis[it]==-1){
-            if(dfs(it,adj,1-col,vis)==false) return false;
+   void  isBiparit(vector<vector<int>>&adj,int node,vector<int>&color,vector<int>&vis,int clr)
+{   vis[node]=1;
+    color[node]=clr;
+    for(auto it:adj[node]){
+        if(!vis[it]){
+           
+            isBiparit(adj,it,color,vis,1-clr);
         }
-        else{
-            if(vis[it]==col){
-                return false;
-            }
-        }
-       
     }
-
-     return true;
     
 }
-
     bool isBipartite(vector<vector<int>>& graph) {
-        vector<int>adj[graph.size()];
-
+        vector<vector<int>>adj(graph.size()+1);
+        
         for(int i=0;i<graph.size();i++){
             for(int j=0;j<graph[i].size();j++){
                 adj[i].push_back(graph[i][j]);
+                adj[graph[i][j]].push_back(i);
+
 
             }
         }
-        vector<int>vis(graph.size(),-1);
+        vector<int>color(graph.size()+1,-1);
+        vector<int>vis(graph.size()+1,0);
         for(int i=0;i<graph.size();i++){
-            if(vis[i]==-1){
-               if( dfs(i,adj,0,vis)==false) return false;;
+            if(!vis[i]){
+               isBiparit(adj,i,color,vis,0);
             }
         }
-        return true;
-
+         for(int i=0;i<graph.size();i++){
+            for(auto it:adj[i])
+            {
+                if(color[it]==color[i])
+                {
+                    return false;
+                }
+            }
+         }
+         return true;
         
     }
 };

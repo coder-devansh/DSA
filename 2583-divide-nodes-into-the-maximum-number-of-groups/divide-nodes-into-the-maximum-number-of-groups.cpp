aad7e74b3e105ea496bae4cb430c1645440654d7
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-01-30 22:48:45 +0530

    Time: 124 ms (85.29%) | Memory: 54.3 MB (51.68%) - LeetSync

--- 2583-divide-nodes-into-the-maximum-number-of-groups/divide-nodes-into-the-maximum-number-of-groups.cpp
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -1,54 +1,44 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    bool isBipartite(vector<vector<int>>& adj, int node, vector<int>& color) {
-        queue<int> q;
-        q.push(node);
-        color[node] = 0;  // Start with color 0
-
-        while (!q.empty()) {
-            int curr = q.front();
+   void  isBiparit(vector<vector<int>>&adj,int node,vector<int>&color,vector<int>&vis,int clr)
+{   vis[node]=1;
+    color[node]=clr;
+    for(auto it:adj[node]){
+        if(!vis[it]){
+           
+            isBiparit(adj,it,color,vis,1-clr);
+        }
+    }
+    
+}
+int getpath(vector<vector<int>>&adj,int src,int n){
+    queue<int>q;
+    vector<int>vis(n+1,0);
+    vis[src]=1;
+    q.push(src);
+    int distance=0;
+    while(!q.empty()){
+        int size=q.size();
+        for(int i=0;i<size;i++){
+            int node=q.front();
             q.pop();
-            for (auto neighbor : adj[curr]) {
-                if (color[neighbor] == -1) {  
-                    color[neighbor] = 1 - color[curr]; // Assign opposite color
-                    q.push(neighbor);
-                } else if (color[neighbor] == color[curr]) {  
-                    return false; // Conflict found → Not bipartite
+            for(auto it:adj[node])
+            {
+                if(!vis[it])
+                {
+                    vis[it]=1;
+                    q.push(it);
+                    
                 }
             }
+            
         }
-        return true;
+        distance++;
     }
-
-    int getpath(vector<vector<int>>& adj, int src, int n) {
-        queue<int> q;
-        vector<int> vis(n + 1, 0);
-        q.push(src);
-        vis[src] = 1;
-        int distance = 0;  // Start from -1 because BFS will increase it after the first level
-
-        while (!q.empty()) {
-            int size = q.size();
-            distance++; // Increase depth at each level
-            for (int i = 0; i < size; i++) {
-                int node = q.front();
-                q.pop();
-                for (auto neighbor : adj[node]) {
-                    if (!vis[neighbor]) {
-                        vis[neighbor] = 1;
-                        q.push(neighbor);
-                    }
-                }
-            }
-        }
-        return distance;
-    }
-
-    int findMaxGroup(vector<vector<int>>& adj, int node, vector<int>& distance, vector<int>& vis) {
-        stack<int> st;
+    return distance;
+}
+int find(vector<vector<int>>&adj,int node,vector<int>&distance,vector<int>&vis){
+    stack<int> st;
         st.push(node);
         vis[node] = 1;
         int max_groups = distance[node];
@@ -64,36 +54,57 @@ public:
                 }
             }
         }
+        cout<<max_groups;
         return max_groups;
-    }
-
+    
+}
     int magnificentSets(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n + 1);
-        for (auto &edge : edges) {
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+        vector<vector<int>>adj(n+1);
+        for(auto &it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
+        vector<int>color(n+1,-1);
+        vector<int>vis(n+1,0);
+        for(int i=1;i<=n;i++){
+            if(!vis[i]){
 
-        vector<int> color(n + 1, -1);
-        for (int i = 1; i <= n; i++) {
-            if (color[i] == -1) {
-                if (!isBipartite(adj, i, color)) return -1;
+               isBiparit(adj,i,color,vis,0);
+        }
+        }
+       for(int i=1;i<=n;i++){
+         
+            for(auto it:adj[i])
+            {
+               if(color[it]==color[i])
+                {
+                    return -1;
+                }
             }
-        }
-
-        vector<int> distance(n + 1, 0);
-        for (int i = 1; i <= n; i++) {
-            distance[i] = getpath(adj, i, n);
-        }
-
-        vector<int> vis(n + 1, 0);
-        int max_groups = 0;
-        for (int i = 1; i <= n; i++) {
-            if (!vis[i]) {
-                max_groups += findMaxGroup(adj, i, distance, vis);
+         }
+        vis.resize(n+1,0);
+         queue<int>q;
+         vector<int>distance(n+1,0);
+         for(int i=1;i<=n;i++){
+            distance[i]=getpath(adj,i,n);
+         }
+         vector<int>visited(n+1,0);
+        int ans=-1;
+        int max_groups=0;
+          
+         for(int i=1;i<=n;i++){
+            if(!visited[i])
+            {
+                ans=find(adj,i,distance,visited);
+              
+                max_groups+=ans;
             }
-        }
+         }
+         return max_groups;
 
-        return max_groups;
+       
+      
+       
+        
     }
 };

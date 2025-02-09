7d10abed1ca3c63466fbf125a760eb6d72cb1d25
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-02-10 00:14:49 +0530

    Time: 137 ms (8.66%) | Memory: 89.3 MB (39.21%) - LeetSync

--- 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.cpp
@@ -4,17 +4,14 @@ public:
         map<int,int>mp;
          long long  count=0;
         for(int i=0;i<nums.size();i++){
-            
+                       count+=i-(mp[nums[i]-i]);
                      mp[nums[i]-i]++;
-                     count+=i-(mp[nums[i]-i]);
+                    
             
             
         }
        
-        for(auto it:mp){
-            count+=it.second;
-
-        }
+        
         if(mp.size()==1){
             return 0;
         }

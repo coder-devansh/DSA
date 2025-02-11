5b5949e9b604f70b4b57e6887ac4ee0fa89b2be3
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-02-12 00:05:20 +0530

    Time: 1303 ms (5.14%) | Memory: 10.4 MB (34.62%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
     int repeatedStringMatch(string a, string b) {
-        
-        map<char,int>mp;
-        map<char,int>first;
-        for(int i=0;i<a.size();i++){
-            mp[a[i]]++;
-        }
-        for(int i=0;i<b.size();i++){
-            if(mp[b[i]]==0){
-                return -1;
-            }
-        }
-        int count=0;
         string s="";
+        int count=0;
         while(s.find(b)==string::npos && s.size()<=1e4){
             s+=a;
             count++;
-
-           
         }
         if(s.find(b)==string::npos){
             return -1;

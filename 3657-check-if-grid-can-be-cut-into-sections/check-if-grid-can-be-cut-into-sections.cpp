e19fb607fc40480582539de05a43c42e137a05b8
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-03-26 02:20:15 +0530

    Time: 460 ms (13.68%) | Memory: 235.7 MB (46.19%) - LeetSync

---
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+bool merge(vector<pair<int,int>>Intervals){
+    vector<pair<int,int>>ans;
+    sort(Intervals.begin(),Intervals.end());
+    for(int i=0;i<Intervals.size();i++){
+        auto first=Intervals[i];
+        if(ans.empty()) ans.push_back({first.first,first.second});
+        else if(ans[ans.size()-1].second>first.first)
+        {
+            int node=min(ans[ans.size()-1].first,first.first);
+            int val=max(ans[ans.size()-1].second,first.second);
+            ans.pop_back();
+            ans.push_back({node,val});
+            
+        }else if(ans[ans.size()-1].second<=first.first){
+            ans.push_back({first.first,first.second});
+        }
+
+    }
+    
+    for(auto it:ans) cout<<it.first<<" "<<it.second<<endl;
+    return ans.size()>=3;
+}
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        vector<pair<int,int>>x_interval;
+        vector<pair<int,int>>y_interval;
+        for(auto it:rectangles){
+            x_interval.push_back({it[0],it[2]});
+            y_interval.push_back({it[1],it[3]});
+        }
+        cout<<"X"<<endl;
+        bool left=merge(x_interval);
+        cout<<'Y'<<endl;
+        bool right=merge(y_interval);
+       return left || right;
+        
+        
+      
+
+        
+    }
+};

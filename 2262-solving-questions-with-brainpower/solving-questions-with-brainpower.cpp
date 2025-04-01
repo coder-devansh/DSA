eac278ee4ae267968736b9935e9affc30aee26aa
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-04-01 15:54:30 +0530

    Time: 23 ms (10.33%) | Memory: 170.3 MB (5.00%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,36 +1,21 @@
-long long dp[100000];
-
 class Solution {
 public:
-
-
-long long  find(int i,vector<vector<int>>&questions,int score){
-   if(i>=questions.size()){
-    return 0;
-   }
-   if(dp[i]!=-1){
-    return dp[i];
-   }
-    
-    long long  right=questions[i][0]+find(i+questions[i][1]+1,questions,score);
-    long long  left=find(i+1,questions,score);
-   
+vector<long long >dp;
+long long Points(int i,vector<vector<int>>&questions){
+    if(i>=questions.size()){
+        return 0;
+    }
+    if(dp[i]!=-1) return dp[i];
+    long long  left=questions[i][0]+Points(i+questions[i][1]+1,questions);
+    long long  right=Points(i+1,questions);
     return dp[i]=max(left,right);
 }
     long long mostPoints(vector<vector<int>>& questions) {
         int n=questions.size();
-      memset(dp, -1, n*sizeof(long long));
-            long long ans=find(0,questions,0);
+
+        dp.resize(1e5,-1);
         
-    
-        return ans;
-        
-    
-
-
-
-        
-
+        return Points(0,questions);
         
     }
 };

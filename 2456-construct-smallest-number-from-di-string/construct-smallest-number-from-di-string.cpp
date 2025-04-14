409a7229fe11ae1dbb1b5b4ca5e38da194f9513e
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-04-14 12:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (23.11%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     string smallestNumber(string pattern) {
-        string result = "";
-
-        // Start building the sequence by calling the helper function
-        buildSequence(0, 0, pattern, result);
-        // Reverse the final result
-        reverse(result.begin(), result.end());
-        return result;
-    }
-
-    // Recursively build the sequence
-    int buildSequence(int currentIndex, int currentCount, string pattern,
-                      string& result) {
-        if (currentIndex != pattern.size()) {
-            if (pattern[currentIndex] == 'I')
-                // If 'I', increment the count and move to the next index
-                buildSequence(currentIndex + 1, currentIndex + 1, pattern,
-                              result);
-            else
-                // If 'D', keep the count and move to the next index
-                currentCount = buildSequence(currentIndex + 1, currentCount,
-                                             pattern, result);
+          stack<int>st;
+        string res="";
+        for(int i=0;i<=pattern.size();i++){
+            st.push(i+1);
+            if(i==pattern.size() || pattern[i]=='I'){
+                while(!st.empty()){
+                    res+=to_string(st.top());
+                    st.pop();
+                }
+            }
         }
-
-        result += to_string(currentCount + 1);
-        // Return the next count for the sequence
-        return currentCount + 1;
+        return res;
+        
+        
     }
 };

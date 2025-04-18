9b46634eea68237c046814b95a3b02f43b5ad5ed
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-04-19 01:25:04 +0530

    Time: 6 ms (34.43%) | Memory: 11.1 MB (23.52%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+string find(int n,string s){
+    if(n==0){
+        return s;
+    }
+    int count=1;
+    string ans="";
+    char ch=s[0];
+    for(int i=0;i<s.size()-1;i++)
+    {
+       
+        if(ch==s[i+1]){
+            count++;
+        }else if(ch!=s[i+1])
+        {
+            ans+=to_string(count);
+            ans+=ch;
+            ch=s[i+1];
+            count=1;
+
+        }
+        
+
+        
+        
+    }
+    ans+=to_string(count);
+    ans+=ch;
+
+    return find(n-1,ans);
+    
+}
+    string countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        if(n==2)return "11";
+        if(n==3) return "21";
+        return find(n-3,"21");
+      
+        
+        
+    }
+};

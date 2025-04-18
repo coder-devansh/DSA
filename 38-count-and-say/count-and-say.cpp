0d57612b9c1074e1cdb4eee707d62545fe53f3ae
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-04-19 01:35:11 +0530

    Time: 4 ms (56.34%) | Memory: 9.9 MB (60.76%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -1,9 +1,47 @@
 class Solution {
 public:
-string find(int n,string s){
-    if(n==0){
-        return s;
-    }
+// string find(int n,string s){
+//     if(n==0){
+//         return s;
+//     }
+//     int count=1;
+//     string ans="";
+//     char ch=s[0];
+//     for(int i=0;i<s.size()-1;i++)
+//     {
+       
+//         if(ch==s[i+1]){
+//             count++;
+//         }else if(ch!=s[i+1])
+//         {
+//             ans+=to_string(count);
+//             ans+=ch;
+//             ch=s[i+1];
+//             count=1;
+
+//         }
+        
+
+        
+        
+//     }
+//     ans+=to_string(count);
+//     ans+=ch;
+
+//     return find(n-1,ans);
+    
+// }
+    string countAndSay(int n) {
+        if(n==1){
+            return "1";
+        }
+        if(n==2)return "11";
+        if(n==3) return "21";
+        int val=n-3;
+        string s="21";
+
+ while(val>0){
+    
     int count=1;
     string ans="";
     char ch=s[0];
@@ -27,17 +65,11 @@ string find(int n,string s){
     }
     ans+=to_string(count);
     ans+=ch;
+    s=ans;
+    val--;
 
-    return find(n-1,ans);
-    
-}
-    string countAndSay(int n) {
-        if(n==1){
-            return "1";
         }
-        if(n==2)return "11";
-        if(n==3) return "21";
-        return find(n-3,"21");
+        return s;
       
         
         

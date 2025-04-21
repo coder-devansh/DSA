0bf834f6aa5095f230d2696f21d31390d5d86bd7
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-04-22 01:10:29 +0530

    Time: 15 ms (5.47%) | Memory: 134.9 MB (5.47%) - LeetSync

--- 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
+++ 2249-count-the-hidden-sequences/count-the-hidden-sequences.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int numberOfArrays(vector<int>& difference, int lower, int upper) {
-        int maxi=*max_element(difference.begin(),difference.end());
-        int mini=*min_element(difference.begin(),difference.end());
         vector<long long>prefix;
         prefix.push_back(3);
+        long long maxi=3;
+        long long  mini=3;
         for(int i=0;i<difference.size();i++){
             long long  value=prefix[prefix.size()-1]+difference[i];
             prefix.push_back(value);
+            maxi=max(maxi,value);
+            mini=min(mini,value);
             
         }
-        sort(prefix.begin(),prefix.end());
-        for(auto it:prefix) cout<<it<<" ";
-        if(prefix.size()==1){
-            return (upper-lower+1)-prefix[0];
-        }
-        int count= (upper-lower+1)-(prefix[prefix.size()-1]-prefix[0]);
-        if(count<0) return 0;
+        int count= (upper-lower+1)-(maxi-mini);
+        if(count<0)return 0;
         return count;
         
     }

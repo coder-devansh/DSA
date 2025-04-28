ca5ec9825ca3b3ab05f049250e4ddbc5bf9bc4c9
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-04-28 19:13:11 +0530

    Time: 8 ms (20.95%) | Memory: 98.9 MB (76.24%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-        long long ans=0,sum=0;
-        int l=0, r=0;
+        long long ans=0;
+        int r=0;
+        int l=0;
+        long long sum=0;
         while(r<nums.size()){
             sum+=nums[r];
-            while(l<=r and sum*(r-l+1)>=k){
+            while(l<=r && sum*(r-l+1)>=k){
                 sum-=nums[l];
                 l++;
             }
-            ans+= (r-l+1);
+            ans+=r-l+1;
             r++;
         }
         return ans;
+        
     }
 };

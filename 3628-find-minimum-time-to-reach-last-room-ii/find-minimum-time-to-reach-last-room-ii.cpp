595f666196fd177772abb593ce04e824ed8c3680
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-05-08 20:50:30 +0530

    Time: 380 ms (58.86%) | Memory: 103.4 MB (64.13%) - LeetSync

--- 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -3,81 +3,58 @@ public:
     int minTimeToReach(vector<vector<int>>& moveTime) {
         int n=moveTime.size();
         int m=moveTime[0].size();
-        int vis[n][m];
-        int turn=0;
-        vector<vector<int>>dis(n,vector<int>(m,INT_MAX));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                vis[i][j]=0;
-            }
-        }
-      
-        
-        vector<pair<int,int>>adj[n];
-       priority_queue<pair<pair<int,int>,pair<int,int>>,vector<pair<pair<int,int>,pair<int,int>>>,greater<pair<pair<int,int>,pair<int,int>>>>q; 
-        
-        int min_time=INT_MAX;
-        q.push({{0,2},{0,0}});
-        vis[0][0]=1;
-        dis[0][0]=0;
+        vector<vector<int>>dist(n+1,vector<int>(m+1,INT_MAX));
+         priority_queue<
+        pair<pair<int, int>, pair<int, int>>,
+        vector<pair<pair<int, int>, pair<int, int>>>,
+        greater<>
+    > q;
+
+        q.push({{0,1},{0,0}});
+        dist[0][0]=0;
+       
+        int r[4]={-1,0,1,0};
+        int c[4]={0,-1,0,1};
         while(!q.empty()){
             auto it=q.top();
             q.pop();
-            int x=it.second.first;
-            int y=it.second.second;
             int time=it.first.first;
-            int len=it.first.second;
-            if(x==n-1 && y==m-1)
-            {
-                return time;
-            }
+            int index=it.first.second;
+            int row=it.second.first;
+            int col=it.second.second;
             
-            int row[]={-1,0,1,0};
-            int col[]={0,1,0,-1};
             for(int i=0;i<4;i++){
-                int finalx=x+row[i];
-                int finaly=y+col[i];
-              if(finalx>=0 && finalx<moveTime.size() && finaly>=0 && finaly<moveTime[0].size())
-              {
-                int diff=0;
-                if((len==2)){
-                    
-    
-                      diff=max(time+1,moveTime[finalx][finaly]+1);
-                      if(diff<dis[finalx][finaly])
+                int new_row=row+r[i];
+                int new_col=col+c[i];
+                if(new_row>=0 && new_col>=0 && new_row<n && new_col<m)
                 {
+                    int diff=0;
+                    if(index%2==0){
+                          diff=max(time,moveTime[new_row][new_col])+2;
+
+                    }else{
+                        diff=max(time,moveTime[new_row][new_col])+1;
+                    }
+                  
+
                     
-                    dis[finalx][finaly]=diff;
-                    q.push({{diff,0},{finalx,finaly}});
-                }
-
-                }
-                else{
                     
-                       diff=max(time+2,moveTime[finalx][finaly]+2);
-                 if(diff<dis[finalx][finaly])
-                {
-                    turn++;
-                    dis[finalx][finaly]=diff;
-                    q.push({{diff,2},{finalx,finaly}});
+                    if(diff<dist[new_row][new_col]){
+                        
+                       
+                        dist[new_row][new_col]=diff;
+                      
+                        q.push({{diff,index+1},{new_row,new_col}});
+                    }
+                    
+                   
+                   
                 }
-
-                }
-              
-               
-                
-              }
-
-
+                 
             }
-
-
-
-
         }
-       
+        return dist[n-1][m-1];
+
         
-        
-        return -1;
     }
 };

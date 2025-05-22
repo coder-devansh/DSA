d62cb51f047304dcdf1365e13e5520f3a7294080
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-05-22 19:49:52 +0530

    Time: 410 ms (23.19%) | Memory: 500 MB (5.19%) - LeetSync

--- 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
-    vector<vector<int>> dp;
-
-    int find(int i, int target, int x) {
-        int power = pow(i, x);
-        if (power > target) return 0;
-        if (power == target) return 1;
-
-        if (dp[i][target] != -1) return dp[i][target];
-
-        long long take = find(i + 1, target - power, x);
-        long long skip = find(i + 1, target, x);
-
-        return dp[i][target] = (take + skip) % MOD;
-    }
-
+vector<vector<long long >>dp;
+int mod=1e9+7;
+long long  find(int i,int target,int x,int dest){
+   int power=pow(i,x);
+   if(power>target){
+    return 0;
+   }
+   if(power==target){
+    return 1;
+   }
+   if(dp[i][target]!=-1){
+    return dp[i][target];
+   }
+   
+    int left=find(i+1,target-power,x,dest);
+    int right=find(i+1,target,x,dest);
+    return dp[i][target]=(left+right)%mod;
+}
     int numberOfWays(int n, int x) {
-        int maxBase = pow(n, 1.0 / x);  // max value of i such that i^x <= n
-        dp.resize(maxBase + 2, vector<int>(n + 1, -1));
-        return find(1, n, x);
+        
+        int dest=sqrt(n);
+        dp.resize(n+3,vector<long long >(n+1,-1));
+        return find(1,n,x,dest);
+        
     }
 };

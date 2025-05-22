d3f684154c96c7d73ac6f4c15ac3c7eac4a04672
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-05-22 18:42:27 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (44.33%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-vector<int>dp;
-int find(int i,vector<int>nums,int target){
-    if(target==0){
-        return 1;
+vector<unsigned int >dp;
+// int find(int i,vector<int>nums,int target){
+//     if(target==0){
+//         return 1;
 
-    }
-    if(target<0) return 0;
-    if(dp[target]!=-1){
-        return dp[target];
-    }
-    int ways=0;
-    for(int  num:nums){
-        ways+=find(i,nums,target-num);
-    }
-    return dp[target]=ways;
-}
+//     }
+//     if(target<0) return 0;
+//     if(dp[target]!=-1){
+//         return dp[target];
+//     }
+//     int ways=0;
+//     for(int  num:nums){
+//         ways+=find(i,nums,target-num);
+//     }
+//     return dp[target]=ways;
+// }
     int combinationSum4(vector<int>& nums, int target) {
-        dp.resize(target+1,-1);
-        return find(0,nums,target);
+        dp.resize(target+1,0);
+        // return find(0,nums,target);
+        dp[0]=1;
+        for(int t=1;t<=target;t++){
+            for(int num:nums){
+                if(t>=num){
+                    dp[t]+=dp[t-num];
+
+                }
+            }
+        }
+        return dp[target];
+
         
     }
 };

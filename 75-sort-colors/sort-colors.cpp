6b9a372cf2481fa3c988bd2dfb32442866ed1e71
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-06-04 21:39:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.77%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        map<int,int>mp;
+        for(auto it:nums){
+            mp[it]++;
+
+        }
+        nums.clear();
+        for(auto it:mp){
+            for(int i=0;i<it.second;i++){
+                nums.push_back(it.first);
+
+            }
+        }
         
     }
 };

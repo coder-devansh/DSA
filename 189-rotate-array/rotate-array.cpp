dda2e1ecbce763f537b85f3ddb7cc0aa505f23e6
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-06-05 21:54:03 +0530

    Time: 3 ms (9.97%) | Memory: 29.5 MB (57.22%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
+// rotation in place;
     void rotate(vector<int>& nums, int k) {
-        vector<int>left;
-        vector<int>right;
-        if(nums.size()==1){
-            return ;
-        }
         k=k%nums.size();
-       for(int i=nums.size()-k;i<nums.size();i++){
-        right.push_back(nums[i]);
-       }
-       for(int i=0;i<nums.size()-k;i++){
-        left.push_back(nums[i]);
-       }
-       nums.clear();
-       for(int i=0;i<right.size();i++){
-        nums.push_back(right[i]);
-       }
-       for(int i=0;i<left.size();i++){
-        nums.push_back(left[i]);
-       }
-        
-
+        reverse(nums.begin(),nums.begin()+nums.size()-k);
+        reverse(nums.begin()+nums.size()-k,nums.end());
+          reverse(nums.begin(),nums.end());
+      
         
     }
 };

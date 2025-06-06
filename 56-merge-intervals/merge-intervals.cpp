b98e4a95198c1dffedba32efd672185c9fe8e2f5
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-06-06 09:47:39 +0530

    Time: 3 ms (89.89%) | Memory: 23.8 MB (59.21%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& Intervals) {
-         vector<vector<int>>ans;
-    sort(Intervals.begin(),Intervals.end());
-    for(int i=0;i<Intervals.size();i++){
-        auto first=Intervals[i];
-        if(ans.empty()) ans.push_back({first[0],first[1]});
-        else if(ans[ans.size()-1][1]>=first[0])
-        {
-            int node=min(ans[ans.size()-1][0],first[0]);
-            int val=max(ans[ans.size()-1][1],first[1]);
-            ans.pop_back();
-            ans.push_back({node,val});
-            
-        }else if(ans[ans.size()-1][1]<first[0]){
-            ans.push_back({first[0],first[1]});
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>ans;
+        ans.push_back({intervals[0][0],intervals[0][1]});
+        for(int i=1;i<intervals.size();i++){
+            if(intervals[i][0]<=ans[ans.size()-1][1]){
+                ans[ans.size()-1][1]=max(ans[ans.size()-1][1],max(intervals[i][0],intervals[i][1]));
+            }else{
+                ans.push_back(intervals[i]);
+            }
         }
-
-    }
-    return ans;
+        return ans;
         
     }
 };

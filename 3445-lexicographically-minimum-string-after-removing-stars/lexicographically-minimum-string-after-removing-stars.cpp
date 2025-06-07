f5a9711653401869c9a464d18372b22fa648a6d8
coder-devansh <159816253+coder-devansh@users.noreply.github.com>
2025-06-07 20:40:19 +0530

    Time: 159 ms (35.71%) | Memory: 37.1 MB (33.44%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,33 +1,23 @@
-struct Compare {
-    bool operator()(pair<int, int>& a, pair<int, int>& b) {
-        if (a.first != b.first) {
-            return a.first > b.first; // min-heap by value
-        } else {
-            return a.second < b.second; // if same value, higher insertion time first
-        }
-    }
-};
+// struct Compare {
+//     bool operator()(pair<int, int>& a, pair<int, int>& b) {
+//         if (a.first != b.first) {
+//             return a.first > b.first; // min-heap by value
+//         } else {
+//             return a.second < b.second; // if same value, higher insertion time first
+//         }
+//     }
+// };
 
 class Solution {
 public:
-
-void erase(char str,string&s,vector<int>&vis,int i){
-    for(int j=i;j>=0;j--){
-        if(s[j]==str && !vis[j]){
-            vis[j]=1;
-            break;
-
-        }
-    }
-}
     string clearStars(string s) {
-      priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> st;
+      priority_queue<pair<int, int>, vector<pair<int, int>>> st;
         string ans="";
         vector<int>vis(s.size(),0);
         for(int i=0;i<s.size();i++){
             if(s[i]=='*'){
                 if(!st.empty()){
-                   vis[st.top().second]=1;
+                   s[st.top().second]='*';
                     st.pop();
                     
                 }
@@ -35,11 +25,7 @@ void erase(char str,string&s,vector<int>&vis,int i){
 
             }
             
-                st.push({s[i],i});
-            
-           
-                
-
+                st.push({-s[i],i});
         }
         if(st.empty()){
             return "";

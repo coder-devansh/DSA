5482d3e803ee5de575016d7a46684a8175a0eea0
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-06-16 19:17:48 +0530

    Time: 47 ms (16.29%) | Memory: 42.7 MB (15.40%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
 vector<vector<int>>result;
-void find(int i,vector<int>&candidates,int target,vector<int>&ans){
+void find(int i,vector<int>&nums,int target,vector<int>temp){
     if(target==0){
-        result.push_back(ans);
-        
+        result.push_back(temp);
+        return;
     }
     if(target<0){
         return;
     }
-
-    for(int j=i;j<candidates.size();j++){
-        ans.push_back(candidates[j]);
-        find(j,candidates,target-candidates[j],ans);
-        ans.pop_back();
-
+    if(i>=nums.size())return;
+    if(target>=nums[i]){
+        temp.push_back(nums[i]);
+        find(i,nums,target-nums[i],temp);
+        temp.pop_back();
     }
-    
+    find(i+1,nums,target,temp);
 }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
-        vector<int>ans;
-        find(0,candidates,target,ans);
+        vector<int>temp;
+        find(0,candidates,target,temp);
         return result;
         
     }

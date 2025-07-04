f3ccbf745d2aa7b0c4779f6a1fcb73d7441d45d2
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-07-04 17:09:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (67.58%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        bool found=false;
+        int ind=-1;
+            for( int i=nums.size()-2;i>=0;i--){
+                if( nums[i+1]>nums[i]){
+                    ind=i;
+                    break;
+                }
+            }
+            if(ind==-1){
+                reverse(nums.begin(),nums.end());
+                return;
+            }
+            for(int j=nums.size()-1;j>=ind;j--){
+                if(nums[ind]<nums[j]){
+                    swap(nums[j],nums[ind]);
+                    break;
+                }
+
+            }
+            reverse(nums.begin()+ind+1,nums.end());
+
         
     }
 };

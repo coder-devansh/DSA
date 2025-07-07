5801a7fd8f3605ba45996fb9005678c82e6cfdab
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-07-08 02:40:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (99.65%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-vector<vector<int>>dp;
-bool find(int i,string s,int count_open ){
-    if(count_open<0)return false;
-    if(i==s.size()){
-        return (count_open==0);
-    }
-   if(dp[i][count_open]!=-1)return dp[i][count_open];
-   
-    if(s[i]=='('){
-         return dp[i][count_open]=find(i+1,s,count_open+1);
-    }
-    if(s[i]==')'){
-        return dp[i][count_open]=find(i+1,s,count_open-1);
-    }
-
-
-        bool c=find(i+1,s,count_open+1);
-        bool d=find(i+1,s,count_open-1);
-        bool e=find(i+1,s,count_open);
-    
-    return dp[i][count_open]=c||d||e;
-}
     bool checkValidString(string s) {
-        dp.resize(s.size(),vector<int>(s.size(),-1));
-        return find(0,s,0);
+        int mini=0;
+        int maxi=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                mini++;
+                maxi++;
+            }
+            if(s[i]==')'){
+                if(maxi-1<0)return false;
+                mini=(mini-1<0?mini:mini-1);
+                maxi=maxi-1;
+            }
+            if(s[i]=='*'){
+                mini=(mini-1<0?mini:mini-1);
+                maxi=maxi+1;
+            }
+        }
+        if(mini==0 || maxi==0){
+            return true;
+        }
+        return false;
         
     }
 };

00860af5fbeb610f2a5874da8ea2d5c25adfb216
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-08-01 07:19:33 +0530

    Time: 21 ms (9.53%) | Memory: 37.3 MB (37.61%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,50 +1,38 @@
 class Solution {
 public:
-long long   dfs(int src,vector<vector<pair<int,int>>>&adj,vector<int>&vis,int n){
-    int MOD=1e9+7;
-  priority_queue<pair<long long , long long >, vector<pair<long long , long long >>, greater<pair<long long , long long>>> pq;
-    vector<long long > dist(n + 1, LLONG_MAX);
-    vector<int> ways(n + 1, 0);
-
-    pq.push({0, src});
-    dist[src] = 0;
-    ways[src] = 1;
-
-    while (!pq.empty()) {
-        long long  d = pq.top().first;
-        int node = pq.top().second;
-        pq.pop();
-
-        if (d > dist[node]) continue;
-
-        for (auto it : adj[node]) {
-            int nextNode = it.first;
-            long long  edw = it.second;
-
-            if (dist[node] + edw < dist[nextNode]) {
-                dist[nextNode] = dist[node] + edw;
-                pq.push({dist[nextNode], nextNode});
-                ways[nextNode] = ways[node]; // New shortest path found
-            } else if (dist[node] + edw == dist[nextNode]) {
-                ways[nextNode] = (ways[nextNode] + ways[node]) % MOD; // Multiple shortest paths
+    int countPaths(int n, vector<vector<int>>& roads) {
+        priority_queue<pair<long long ,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>q;
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto it:roads){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
+        }
+        vector<long long > dist(n + 1, LLONG_MAX);
+        vector<int>ways(n,0);
+        q.push({0,0});
+        ways[0]=1;
+        dist[0]=0;
+        int mod=(int)(1e9+7);
+        while(!q.empty()){
+            auto it=q.top();
+            q.pop();
+            long long  distance=it.first;
+            int node=it.second;
+            if (distance > dist[node]) continue;
+            for(auto it:adj[node]){
+                int road_node=it.first;
+                long long  road_distance=it.second;
+                if(dist[node]+road_distance<dist[road_node]){
+                    dist[road_node]=dist[node]+road_distance;
+                    ways[road_node]=ways[node];
+                    q.push({dist[road_node],road_node});
+                }else if(dist[node]+road_distance==dist[road_node]){
+                    ways[road_node]=(ways[road_node]+ways[node])%mod;
+                }
             }
         }
-    }
-    return (long long )ways[n-1];
-}
-    int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>>vec(n);
-         vector<vector<pair<int,int>>>adj(n);
-        for(auto it:roads){
-            vec[it[0]].push_back({it[1],it[2]});
-            vec[it[1]].push_back({it[0],it[2]});
-        }
-        vector<int>vis(n,0);
-        long long  maxi=dfs(0,vec,vis,n);
-        if(maxi==0) return 1;
-        return maxi;
-        
-        
+        return ways[n-1];
+
         
     }
 };

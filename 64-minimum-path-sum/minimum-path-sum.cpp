f9a24bff8177ed91c8321993a44157975e161284
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-08-19 11:39:43 +0530

    Time: 0 ms (100.00%) | Memory: 16.2 MB (62.23%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+// recursive way
+
 int find(int i,int j,int n,int m,vector<vector<int>>&grid,vector<vector<int>>&dp){
     if(i==n-1 && j==m-1){
         return grid[i][j];
@@ -16,8 +18,16 @@ int find(int i,int j,int n,int m,vector<vector<int>>&grid,vector<vector<int>>&dp
         
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return find(0,0,n,m,grid,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,1e9));
+        dp[n-1][m-1]=grid[n-1][m-1];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                 if (i == n - 1 && j == m - 1) continue; // already filled
+                dp[i][j]=min(dp[i+1][j],dp[i][j+1])+grid[i][j];
+            }
+        }
+        return dp[0][0];
+        // return find(0,0,n,m,grid,dp);
 
         
     }

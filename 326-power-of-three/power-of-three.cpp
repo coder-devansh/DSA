be3bd1b768ea1f584e6d87b8793323930422e8ba
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-08-21 16:17:18 +0530

    Time: 4 ms (48.06%) | Memory: 8.7 MB (97.25%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0)return false;
-        long long value=pow(3,19);
-        if(value % n==0)return true;
-        return false;
+        return n>0?(1162261467 % n==0):false;
         
     }
 };

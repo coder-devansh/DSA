aff01821c6afabd3c36a9e8b90e0e7ca668ece42
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-08-27 09:17:42 +0530

    Time: 254 ms (31.41%) | Memory: 62 MB (8.94%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,33 +1,19 @@
 class Solution {
 public:
+int no_of_ways(int i,int sum,vector<int>&nums,int target,map<pair<int,int>,int>&mp){
+    if(i==nums.size()){
+        return sum==target;
+    }
+    if(mp.find({i,sum})!=mp.end())return mp[{i,sum}];
+    int left=no_of_ways(i+1,sum+nums[i],nums,target,mp);
+    int right=no_of_ways(i+1,sum-nums[i],nums,target,mp);
+    return mp[{i,sum}]=left+right;
+    
+
+}
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int offset = 1000; // To handle negative sums, we shift the range by 1000
-        int maxSum = 2000; // Total possible range [-1000, 1000] becomes [0, 2000]
-
-        // Initialize DP table with size (n+1) x (maxSum+1) and set all values to 0
-        vector<vector<int>> dp(n + 1, vector<int>(maxSum + 1, 0));
-
-        // Base case: At index 0, sum of 0 (shifted by offset) can be achieved in 1 way
-        dp[0][offset] = 1;
-
-        // Fill the DP table iteratively
-        for (int i = 1; i <= n; i++) {
-            for (int currSum = 0; currSum <= maxSum; currSum++) {
-                if (dp[i - 1][currSum] > 0) { // If there's a way to achieve this sum
-                    // Add the current number
-                    if (currSum + nums[i - 1] <= maxSum) {
-                        dp[i][currSum + nums[i - 1]] += dp[i - 1][currSum];
-                    }
-                    // Subtract the current number
-                    if (currSum - nums[i - 1] >= 0) {
-                        dp[i][currSum - nums[i - 1]] += dp[i - 1][currSum];
-                    }
-                }
-            }
-        }
-
-        // The answer is the number of ways to achieve the target shifted by offset
-        return dp[n][target + offset];
+     map<pair<int,int>,int>mp;
+        return no_of_ways(0,0,nums,target,mp);
+        
     }
 };

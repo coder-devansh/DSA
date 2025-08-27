aca54d1b125d2f75f8ed69e3decdd3247fedc480
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-08-27 09:01:13 +0530

    Time: 3 ms (7.36%) | Memory: 10.9 MB (40.70%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,51 +1,29 @@
 class Solution {
 public:
-vector<int>dp;
-vector<int>dp1;
-int fun1(int n,vector<int>&nums){
-    if(n==0){
-        return nums[0];
-    }
-     if(n<0){
+int amount_stolen(int i,int n,vector<int>&nums,vector<int>&dp){
+    if(i>=n){
         return 0;
     }
-    if(dp[n]!=-1)
-    {
-        return dp[n];
-    }
-    
-    int pick=nums[n]+fun1(n-2,nums);
-    int notpick=fun1(n-1,nums);
-    return dp[n]=max(pick,notpick);
-}
-int fun(int n,vector<int>&nums){
-      if(n==1){
-        return nums[1];
-    }
-    if(n<1){
-        return 0;
-    }
-    if(dp1[n]!=-1){
-        return dp1[n];
-    }
-  
-    int pick=nums[n]+fun(n-2,nums);
-    int notpick=fun(n-1,nums);
-    return dp1[n]=max(pick,notpick);
+    if(dp[i]!=-1)return dp[i];
+
+
+    int stolen_money=nums[i]+amount_stolen(i+2,n,nums,dp);
+    int not_pick=amount_stolen(i+1,n,nums,dp);
+
+    return dp[i]=max(stolen_money,not_pick);
+
+
 }
     int rob(vector<int>& nums) {
-        if(nums.size()==0){
-            return 0;
+        if(nums.size()==1)return nums[0];
+        nums.push_back(nums[0]);
+        int n=nums.size();
+        vector<int>dp1(nums.size()+1,-1);
+         int amount_1=amount_stolen(1,n-1,nums,dp1);
+         vector<int>dp2(nums.size()+1,-1);
+         int amount_2=amount_stolen(2,n,nums,dp2);
+         return max(amount_1,amount_2);
 
-        }
-        if(nums.size()==1){
-            return nums[0];
-        }
-      dp.resize(nums.size(),-1);
-      int left= fun1(nums.size()-2,nums);
-      dp1.resize(nums.size(),-1); 
-      int right= fun(nums.size()-1,nums);
-       return max(left,right);       
-
+        
     }
 };

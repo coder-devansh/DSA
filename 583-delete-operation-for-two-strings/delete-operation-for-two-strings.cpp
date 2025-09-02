27228a66569523fe7ac3d34170cdb848101f5816
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-09-02 09:36:19 +0530

    Time: 676 ms (5.00%) | Memory: 377.7 MB (5.00%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
+int find_common(int i,int j,string word1,string word2,vector<vector<int>>&dp){
+
+   if(i==word1.size() || j==word2.size())return 0;
+   if(dp[i][j]!=-1)return dp[i][j];
+    if(word1[i]==word2[j]){
+        return dp[i][j]=1+find_common(i+1,j+1,word1,word2,dp);
+    }
+
+    return dp[i][j]=max(find_common(i+1,j,word1,word2,dp),find_common(i,j+1,word1,word2,dp));
+    
+}
     int minDistance(string word1, string word2) {
-        vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,0));
-        for(int i=0;i<=word1.size();i++){
-            dp[i][0]=0;
-        }
-        for(int j=0;j<=word2.size();j++){
-            dp[0][j]=0;
-        }
-        for(int i=1;i<=word1.size();i++){
-            for(int j=1;j<=word2.size();j++){
-                if(word1[i-1]==word2[j-1])
-                {
-                    dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
-            }
-        }
-       return (word1.size()-dp[word1.size()][word2.size()]+word2.size()-dp[word1.size()][word2.size()]);
+        vector<vector<int>>dp(word1.size(),vector<int>(word2.size(),-1));
+        int length= find_common(0,0,word1,word2,dp);
+        return word1.size()-length+word2.size()-length;
         
     }
 };

9d364482f647afd6136e407a4daef8dea9bb7f5d
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-09-15 14:27:35 +0530

    Time: 82 ms (5.65%) | Memory: 12 MB (5.65%) - LeetSync

---
+++ 1264-maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        map<char,int>mp;
+        for(auto it:brokenLetters){
+            mp[it]++;
+        }
+        int ans=0;
+        for(int i=0;i<text.size();){
+            map<char,int>mp1;
+            while(i<text.size() && text[i]!=' '){
+                mp1[text[i]]++;
+                i++;
+            }
+            i++;
+            bool found=false;
+            for(auto it:mp1){
+                cout<<it.first<<endl;
+                if(mp.find(it.first)!=mp.end()){
+                   found=true;
+                }
+            }
+            if(!found)ans++;
+        }
+        return ans;
+        
+
+        
+    }
+};

53374fa08256cf05c9617545468bd669baaed783
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-09-21 13:54:15 +0530

    Time: 3 ms (63.42%) | Memory: 13.9 MB (91.21%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-int countPair(int mid,vector<int>nums){
+int countKth(int mid,vector<int>&nums){
     int j=0;
-    int count_pair=0;
+    int count=0;
     for(int i=0;i<nums.size();i++){
-        while(j<nums.size() && nums[j]-nums[i]<=mid){
+        while(j<nums.size() &&  nums[j]-nums[i]<=mid){
             j++;
         }
-        count_pair+=j-i-1;
+        count+=j-i-1;
     }
-    return count_pair;
+    return count;
 }
     int smallestDistancePair(vector<int>& nums, int k) {
         sort(nums.begin(),nums.end());
@@ -17,14 +17,13 @@ int countPair(int mid,vector<int>nums){
         int high=nums[nums.size()-1]-nums[0];
         while(low<high){
             int mid=(low+high)/2;
-            if(countPair(mid,nums)>=k)
-            {
+            if(countKth(mid,nums)>=k){
                 high=mid;
             }else{
                 low=mid+1;
             }
         }
-        
         return low;
+        
     }
 };

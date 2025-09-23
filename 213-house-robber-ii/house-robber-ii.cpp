585926073ba3760999de707053138fb210f3ec9b
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-09-23 12:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (56.74%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-int amount_stolen(int i,int n,vector<int>&nums,vector<int>&dp){
-    if(i>=n){
-        return 0;
-    }
+long long find_money(int i,int n,vector<int>&nums,vector<int>&dp){
+    if(i>n)return 0;
     if(dp[i]!=-1)return dp[i];
-
-
-    int stolen_money=nums[i]+amount_stolen(i+2,n,nums,dp);
-    int not_pick=amount_stolen(i+1,n,nums,dp);
-
-    return dp[i]=max(stolen_money,not_pick);
-
-
+    long long take=nums[i]+find_money(i+2,n,nums,dp);
+    long long not_take=find_money(i+1,n,nums,dp);
+    return dp[i]=max(take,not_take);
 }
     int rob(vector<int>& nums) {
         if(nums.size()==1)return nums[0];
-        nums.push_back(nums[0]);
+        if(nums.size()==2)return max(nums[0],nums[1]);
         int n=nums.size();
-        vector<int>dp1(nums.size()+1,-1);
-         int amount_1=amount_stolen(1,n-1,nums,dp1);
-         vector<int>dp2(nums.size()+1,-1);
-         int amount_2=amount_stolen(2,n,nums,dp2);
-         return max(amount_1,amount_2);
-
+        vector<int>dp(n+1,-1);
+        long long ans1=find_money(0,n-2,nums,dp);
+        dp.assign(n+1,-1);
+        long long ans2=find_money(1,n-1,nums,dp);
+        return max(ans1,ans2);
         
     }
 };

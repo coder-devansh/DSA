2965d80fb8e5072eb3a863a53d6bc412e26c8b3b
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-09-26 23:47:15 +0530

    Time: 62 ms (19.26%) | Memory: 153.3 MB (14.68%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-          deque<int> dq;
-         if(k>nums.size()){
-            return {0};
-         }
-         vector<int>res;
-        for(int i=0;i<nums.size();i++){
-            while(!dq.empty() && nums[dq.back()]<nums[i]){
-                dq.pop_back();
+        priority_queue<pair<int,int>>pq;
+        vector<int>ans;
+        for(int i=0;i<k;i++){
+           pq.push({nums[i],i});
+        }
+        ans.push_back(pq.top().first);
+        for(int i=k;i<nums.size();i++){
+           pq.push({nums[i],i});
+            while(!pq.empty() && (pq.top().second)<=i-k){
+                pq.pop();
             }
-            dq.push_back(i);
-            if(!dq.empty() && dq.front()<=i-k) dq.pop_front();
-            if(i>=k-1) res.push_back(nums[dq.front()]);
-            
+            if(!pq.empty()){
+                ans.push_back(pq.top().first);
+            }
+          
 
         }
-        return res;
-
-
-
-
+        
+        return ans;
         
     }
 };

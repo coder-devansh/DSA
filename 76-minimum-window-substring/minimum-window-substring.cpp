0f46380ab7de0a0fd6feda1149ec1f910bdf8135
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-09-27 16:21:07 +0530

    Time: 22 ms (14.36%) | Memory: 11.3 MB (94.55%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        map<char,int>t_map;
-        for(auto it:t){
-            t_map[it]++;
-        }
-        int required=t_map.size();
-        int formed=0;
-        map<char,int>s_map;
-        int right=0;
-        int left=0;
-        int mini=INT_MAX;
-        string ans;
-        int start=0;
-        
-        while(right<s.size()){
-            char c=s[right];
-            s_map[c]++;
-            if(t_map.count(c) && s_map[c]==t_map[c]){
-                formed++;
+       map<char,int>mp;
+       for(auto it:t){
+        mp[it]++;
+       }
+       int left=0;
+       int right=0;
+       int start=0;
+       int minLength=INT_MAX;
+       int word=t.size();
+       while(right<s.size()){
+        if(mp.find(s[right])!=mp.end()){
+          
+            if(mp[s[right]]>0){
+                word--;
             }
-            while(formed==required){
-                int length=right-left+1;
-                while(length<mini){
-                  
-                   start=left;
-                   mini=length;
+              mp[s[right]]--;
+        }
+        while(word==0){
+            if(right>=left){
+            int len=right-left+1;
+            if(minLength>len){
+                minLength=len;
+                start=left;
+            }
+            }
+            char leftChar=s[left];
+            if(mp.find(leftChar)!=mp.end()){
+                mp[leftChar]++;
+                if(mp[leftChar]>0){
+                    word++;
                 }
-
-                //shrink from the left
-                char ch=s[left];
-                s_map[ch]--;
-                if(s_map[ch]==0)s_map.erase(ch);
-                if(s_map[ch]<t_map[ch])formed--;
-                left++;
             }
-            right++;
-            
-            
+            left++;
+
         }
-
-        return mini==INT_MAX?"":s.substr(start,mini);
-
+        right++;
+        
+       }
+       if(minLength==INT_MAX)return "";
+       return s.substr(start,minLength);
         
     }
 };

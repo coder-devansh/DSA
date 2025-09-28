1e4cd91367809c0d1110b0658604764f25d29a49
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-09-28 23:02:47 +0530

    Time: 4 ms (84.94%) | Memory: 12.4 MB (96.64%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-int longestOnes(string nums, int k,char ch) {
+    int maxConsecutiveAnswers(string s, int k) {
+        int t=0;
+        int f=0;
         int right=0;
         int left=0;
-        map<char,int>mp;
         int maxi=INT_MIN;
-        while(right<nums.size()){
-            mp[nums[right]]++;
-            while(mp[ch]>k){
-                mp[nums[left]]--;
-                if(mp[nums[left]]==0)mp.erase(nums[left]);
+        while(right<s.size()){
+            if(s[right]=='T'){
+                t++;
+            }else{
+                f++;
+            }
+            while(t>k && f>k){
+                if(s[left]=='T'){
+                    t--;
+                }else{
+                    f--;
+                }
                 left++;
             }
-            maxi=max(maxi,right-left+1);
+            if(t<=k | f<=k){
+                maxi=max(maxi,right-left+1);
+            }
             right++;
         }
         return maxi;
-
-        
-    }   
-    int maxConsecutiveAnswers(string answerKey, int k) {
-        int val1=longestOnes(answerKey,k,'T');
-        int val2=longestOnes(answerKey,k,'F');
-        return max(val1,val2);
         
     }
 };

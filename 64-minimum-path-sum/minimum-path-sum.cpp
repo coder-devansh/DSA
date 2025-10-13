51e2d1a4f937b6967589110e8492042a2ba3a7a0
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-10-13 16:19:18 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (14.59%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,33 +1,24 @@
 class Solution {
 public:
-// recursive way
-
-int find(int i,int j,int n,int m,vector<vector<int>>&grid,vector<vector<int>>&dp){
-    if(i==n-1 && j==m-1){
-        return grid[i][j];
-    }
-    if(i<0 || j<0 || i>=n || j>=m){
-        return INT_MAX;
-    }
-    if(dp[i][j]!=-1)return dp[i][j];
-    int left=find(i+1,j,n,m,grid,dp);
-    int right=find(i,j+1,n,m,grid,dp);
-    return dp[i][j]=min(left,right)+grid[i][j];
-}
     int minPathSum(vector<vector<int>>& grid) {
-        
         int n=grid.size();
         int m=grid[0].size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,1e9));
+        vector<vector<long long>>dp(n+1,vector<long long>(m+1,0));
+        for(int i=0;i<grid.size();i++){
+            dp[i][m]=INT_MAX;
+        }
+        for(int j=0;j<grid[0].size();j++){
+            dp[n][j]=INT_MAX;
+        }
         dp[n-1][m-1]=grid[n-1][m-1];
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
-                 if (i == n - 1 && j == m - 1) continue; // already filled
-                dp[i][j]=min(dp[i+1][j],dp[i][j+1])+grid[i][j];
+                if(i==n-1 && j==m-1)continue;
+                dp[i][j]=grid[i][j]+min(dp[i+1][j],dp[i][j+1]);
             }
         }
         return dp[0][0];
-        // return find(0,0,n,m,grid,dp);
+
 
         
     }

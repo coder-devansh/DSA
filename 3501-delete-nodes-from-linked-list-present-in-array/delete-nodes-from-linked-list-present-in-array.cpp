ea93f99dd47556adfaac89a9b699613e179ff3cf
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-11-01 22:06:48 +0530

    Time: 133 ms (25.05%) | Memory: 277.2 MB (13.33%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.cpp
@@ -11,15 +11,13 @@
 class Solution {
 public:
     ListNode* modifiedList(vector<int>& nums, ListNode* head) {
-        map<int,int>mp;
+        unordered_map<int,int>mp;
         for(auto it:nums){
             mp[it]++;
         }
         ListNode*new_Head=new ListNode(-1);
         new_Head->next=head;
         ListNode*temp=new_Head;
-        ListNode*temp1=new ListNode(-1);
-        ListNode*prev=temp1;
         while(temp->next!=NULL){
            int val=temp->next->val;
            if(mp[val]>0){

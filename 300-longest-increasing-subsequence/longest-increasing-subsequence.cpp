4dd7f089ff73393de07321a8d18952320137a2f4
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-11-04 11:17:34 +0530

    Time: 55 ms (67.51%) | Memory: 14.4 MB (48.37%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-int find_length(int i,int len,vector<int>&nums,int prev,vector<vector<int>>&dp){
- if(i==nums.size())return 0;
- if(dp[i][prev+1]!=-1)return dp[i][prev+1];
- int left=0;
-    if(prev==-1 || nums[i]>nums[prev]){
-        left=1+find_length(i+1,len,nums,i,dp);
-    }
-   int right=find_length(i+1,len,nums,prev,dp);
-
-   return dp[i][prev+1]=max(left,right);
-}
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size(),vector<int>(nums.size()+1,-1));
+        vector<int>arr(nums.size(),1);
+        int maxi=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    if(arr[i]<=arr[j]){
+                        arr[i]=arr[j]+1;
+                    }
+                    
+                }
+            }
+            maxi=max(maxi,arr[i]);
+        }
+        return maxi;
 
-       return find_length(0,0,nums,-1,dp);
-
-       
+        
+        
         
     }
 };

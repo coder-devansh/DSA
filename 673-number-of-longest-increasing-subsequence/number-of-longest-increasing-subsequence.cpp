689db8f828e094bb71f7386c9b7330adfe9898cd
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-11-04 14:37:42 +0530

    Time: 29 ms (59.31%) | Memory: 17.7 MB (11.21%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,63 +1,36 @@
 class Solution {
 public:
-vector<vector<int>>dp1;
-vector<vector<vector<int>>>dp2;
-int no_inc(int i,int n,vector<int>&nums,int prev,int inc,int cost){
-    if(cost==inc){
-        return 1;
-    }
-    if(i==n){
-        return 0;
-    }
-    if(dp2[i][prev+1][cost]!=-1)return dp2[i][prev+1][cost];
-    
-    long long left=0;
-    if(prev==-1 || nums[i]>nums[prev]){
-        left=no_inc(i+1,n,nums,i,inc,cost+1);
-    }
-   long long right=no_inc(i+1,n,nums,prev,inc,cost);
-   return dp2[i][prev+1][cost]=left+right;
-}
-int lis(int i,int n,vector<int>&nums,int prev){
-    if(i==n){
-        return 0;
-    }
-    if(dp1[i][prev+1]!=-1)return dp1[i][prev+1];
-    long long left=0;
-    if(prev==-1 || nums[prev]<nums[i]){
-        left=1+lis(i+1,n,nums,i);
-    }
-    long long right=lis(i+1,n,nums,prev);
-    return dp1[i][prev+1]=max(left,right);
-}
     int findNumberOfLIS(vector<int>& nums) {
-        vector<int>forward(nums.size(),1);
-        vector<int>count(nums.size()+1,1);
-        count[1]=1;
+         vector<int>arr(nums.size(),1);
         int maxi=0;
+        int last=0;
+        vector<int>count(nums.size(),1);
         for(int i=0;i<nums.size();i++){
             for(int j=0;j<i;j++){
-                if(nums[i]>nums[j] && 1+forward[j]>forward[i]){
-                   
-                    forward[i]=1+forward[j];
-                    count[i]=count[j];
-                }else if(nums[i]>nums[j] && forward[i]==1+forward[j]){
-                    count[i]+=count[j];
+                if(nums[i]>nums[j]){
+                    if(arr[i]<arr[j]+1){
+                        arr[i]=arr[j]+1;
+                        count[i]=count[j];
+                    }else if(arr[i]==arr[j]+1){
+                        count[i]+=count[j];
+                        arr[i]=arr[j]+1;
+                    }
+                    
                 }
-                
             }
-            maxi=max(maxi,forward[i]);
+            if(maxi<arr[i]){
+                last=i;
+                maxi=arr[i];
+            }
         }
-        if(maxi==1){
-            return nums.size();
+        int result=0;
+        for(int i=0;i<arr.size();i++){
+            if(maxi==arr[i]){
+                result+=count[i];
+            }
         }
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            if(forward[i]==maxi)ans+=count[i];
-        }
-
-        return ans;
         
+       return result;
         
     }
 };

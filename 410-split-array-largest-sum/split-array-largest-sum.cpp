1d106d1ddeeff78be4e65ee804e8fd886dec252e
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-11-17 15:38:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (57.24%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,34 +1,36 @@
 class Solution {
 public:
-bool canPossible(int mid,vector<int>nums,int k){
-    int sum=0;
+bool canbepossible(vector<int>&nums,int mid,int k){
     int count=1;
+    int sum=0;
     for(int i=0;i<nums.size();i++){
         sum+=nums[i];
         if(sum>mid){
-            sum=nums[i];
             count++;
-            if(count>k){
-                return false;
-            }
+            sum=nums[i];
+            if(count>k)return false;
         }
     }
     return true;
 }
+
     int splitArray(vector<int>& nums, int k) {
         int low=*max_element(nums.begin(),nums.end());
         int high=0;
-        for(auto it:nums)high+=it;
-        while(low<high){
-            int mid=(low+(high-low)/2);
-            if(canPossible(mid,nums,k)){
-                high=mid;
+        for(auto it:nums){
+            high+=it;
+        }
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(canbepossible(nums,mid,k)){
+                high=mid-1;
+                ans=mid;
             }else{
                 low=mid+1;
             }
-
         }
-        return low;
+        return ans;
         
     }
 };

41e702f0bc289a98d6a46896f9b80fe6c738420e
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-11-23 19:33:52 +0530

    Time: 15 ms (45.65%) | Memory: 55.2 MB (6.21%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.java
@@ -1,54 +1,22 @@
 class Solution {
-
-    public int maxSumDivThree(int[] nums) {
-       ArrayList<Integer>rem_1=new ArrayList<>();
-       ArrayList<Integer>rem_2=new ArrayList<>();
-       int sum=0;   
-       for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
-            if(nums[i]%3==1){
-                rem_1.add(nums[i]);
-            }else if(nums[i]%3==2){
-                rem_2.add(nums[i]);
-
+    int find(int i,int []nums,int rem,int [][]dp){
+        if(i>=nums.length){
+            if(rem==0){
+                return 0;
             }
-
-
-       }
-       if(sum%3==0){
-        return sum;
-       }
-       Collections.sort(rem_1);
-       Collections.sort(rem_2);
-       int maxi=0;
-       int rem=sum%3;
-       if(rem==1){
-        int val1=Integer.MAX_VALUE;
-        int val2=Integer.MAX_VALUE;
-          if(rem_1.size()>0){
-             val1=rem_1.get(0);
-          }if(rem_2.size()>=2){
-             val2=rem_2.get(0)+rem_2.get(1);
-          }
-          maxi=Math.max(maxi,sum-Math.min(val1,val2));
-          return maxi;
-
-       }
-       if(rem==2){
-        int val1=Integer.MAX_VALUE;
-        int val2=Integer.MAX_VALUE;
-        if(rem_2.size()>0){
-            val1=rem_2.get(0);
+            return Integer.MIN_VALUE;
         }
-        if(rem_1.size()>=2){
-            val2=rem_1.get(0)+rem_1.get(1);
+        if(dp[i][rem]!=-1)return dp[i][rem];
+        int take=nums[i]+find(i+1,nums,(rem+nums[i])%3,dp);
+        int skip=find(i+1,nums,rem,dp);
+        return dp[i][rem]=Math.max(take,skip);
+    }
+    public int maxSumDivThree(int[] nums) {
+        int dp[][]=new int[nums.length+1][3];
+        for(int i=0;i<=nums.length;i++){
+            Arrays.fill(dp[i],-1);
         }
-        maxi=Math.max(maxi,sum-Math.min(val1,val2));
-        return maxi;
-       }
-       return maxi;
-       
-
+        return find(0,nums,0,dp);
         
     }
 }

8ee92efbac9ee01b3bdf35065f89ae9c54ea8c39
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2025-12-25 15:54:05 +0530

    Time: 124 ms (81.67%) | Memory: 120.6 MB (65.00%) - LeetSync

--- 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.cpp
+++ 2175-count-nodes-with-the-highest-score/count-nodes-with-the-highest-score.cpp
@@ -1,49 +1,50 @@
 class Solution {
 public:
-int find_size(vector<vector<int>>&adj,int node,vector<int>&count){
+int dfs(int node,int parent,vector<int>&parents,vector<int>&count,vector<vector<int>>&adj){
     int size=1;
     for(auto it:adj[node]){
-        size+=find_size(adj,it,count);
+        if(it==parent)continue;
+        size+=dfs(it,node,parents,count,adj);
     }
     count[node]=size;
     return size;
-
 }
     int countHighestScoreNodes(vector<int>& parents) {
-        vector<int>count(parents.size()+1,0);
-        
-        vector<vector<int>>adj(parents.size()+1);
-        
-        for(int i=1;i<parents.size();i++){
-            adj[parents[i]].push_back(i);
-            
+        int n=parents.size();
+        vector<vector<int>>adj(n+1);
+        for(int i=0;i<parents.size();i++){
+            if(parents[i]!=-1){
+                adj[parents[i]].push_back(i);
+            }
+
         }
-            find_size(adj,0,count);
+        vector<int>count(n+1,0);//use to store the count of the node in the subtree;
+        dfs(0,-1,parents,count,adj);
         long long max_score=0;
         int ans=1;
-        for(int i=0;i<parents.size();i++)
-        {
-          long long score=1;
-           int remaining=parents.size()-count[i];
-           auto &it=adj[i];
-           for(auto ele:it){
-            score*=count[ele];
-           }
-           if(remaining>0){
-            score*=remaining;
-           }
-           if(max_score==score){
-            ans++;
+        for(int i=0;i<parents.size();i++){
+            
+            vector<int>&list=adj[i];
+            long long score=1;
+            int remaining=n-count[i];
+            for(auto&ele:list){
+                score*=count[ele];
 
-           }else if(max_score<score){
-            max_score=score;
-            ans=1;
-           }
+            }
+            if(remaining>0){
+                score*=remaining;
+            }
+            if(score==max_score){
+                ans++;
+
+            }
+            else if(max_score<score){
+                max_score=score;
+                ans=1;
+            }
 
         }
         return ans;
-
-
         
     }
 };

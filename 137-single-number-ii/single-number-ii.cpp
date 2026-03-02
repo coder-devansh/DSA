224dd31c2294114eaf7587d0e5822b3b29f1c9e6
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2026-03-02 17:54:29 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (35.47%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,23 @@
+// use bit manipulation for finding the correct ans;
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        vector<int>bit(32,0);
+        for(int i=31;i>=0;i--){
+            for(int j=0;j<nums.size();j++){
+                if(((nums[j]>>i) & 1)==1){
+                    bit[i]++;
+                }
+            }
+        }
+        int ans=0;
+        for(int i=31;i>=0;i--){
+            if(bit[i]%3!=0){
+                ans=ans | (1<<i);
+            }
+        }
+        return ans;
+        
+        
+    }
+};

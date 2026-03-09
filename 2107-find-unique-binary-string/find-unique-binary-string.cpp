168cf9b074553e1d471182cbb177a363b390872e
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2026-03-09 16:22:39 +0530

    Time: 1590 ms (5.22%) | Memory: 173.7 MB (10.45%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-vector<string>result;
-void generate(vector<string>nums,map<string,int>mp,string ans){
-    if(ans.size()==nums[0].size()){
-        if(mp.find(ans)==mp.end()){
-            result.push_back(ans);
-        }
-        return;
-    }
-    for(char j='0';j<='1';j++){
-        if(result.size()==1){
-            break;
-        }
-        generate(nums,mp,ans+j);
-
+string find(int i,int n,string str,unordered_map<string,int>&mp){
+    if(i==n){
+        if(mp.find(str)==mp.end())return str;
+        else return "-1";
 
     }
+    auto left=find(i+1,n,str+'0',mp);
+    auto right=find(i+1,n,str+'1',mp);
+    if(left!="-1" )return left;
+    if(right!="-1")return right;
+    return left;
 
-   
 }
-
     string findDifferentBinaryString(vector<string>& nums) {
-        map<string,int>mp;
-        int n;
-        for(auto it:nums){
-            n=it.size();
-            mp[it]++;
-        }
-        generate(nums,mp,"");
-        return result[0];
-
+        unordered_map<string,int>mp;
+        for(auto it:nums)mp[it]++;
+        int n=nums[0].size();
+        return find(0,n,"",mp);
+        
         
     }
 };

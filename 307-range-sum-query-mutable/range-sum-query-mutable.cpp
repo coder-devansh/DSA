aad502a2befbf6d15f07c1fca36093b23d59cf83
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2026-03-11 23:44:21 +0530

    Time: 35 ms (95.77%) | Memory: 180.3 MB (81.86%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,56 +1,41 @@
 class NumArray {
 public:
-vector<int>segmentTree;
-void buildSegmentTree(int i,int l,int r,vector<int>&nums){
-    if(l==r)
-    {
-        segmentTree[i]=nums[l];
-        return;
+   vector<int>bit;
+   vector<int>nums;
+   int N;
+    NumArray(vector<int>& arr) {
+        N=arr.size();
+        nums=arr;
+        bit.resize(nums.size()+1,0);
+        for(int i=0;i<nums.size();i++){
+           add(i+1,nums[i]);
+        }
+        
     }
-    int mid=l+(r-l)/2;
-    buildSegmentTree(2*i+1,l,mid,nums);
-    buildSegmentTree(2*i+2,mid+1,r,nums);
-    segmentTree[i]=segmentTree[2*i+1]+segmentTree[2*i+2];
-}
-void updateSegmentTree(int i,int l,int r,int index,int val){
-    if(l==r){
-        segmentTree[i]=val;
-        return;
-    }
-    int mid=l+(r-l)/2;
-    if(mid>=index){
-        updateSegmentTree(2*i+1,l,mid,index,val);
-    }else{
-        updateSegmentTree(2*i+2,mid+1,r,index,val);
-    }
-    segmentTree[i]=segmentTree[2*i+1]+segmentTree[2*i+2];
-}
-long long  findSum(int i,int left,int right,int l,int r){
-    if(right<l || left>r)return 0;
-    if(left<=l && r<=right)return segmentTree[i];
-    int mid=l+(r-l)/2;
-    long long  left_sum=findSum(2*i+1,left,right,l,mid);
-    long long  right_sum=findSum(2*i+2,left,right,mid+1,r);
-    return left_sum+right_sum;
-
-}
-    NumArray(vector<int>& nums) {
-        int n=nums.size();
-        segmentTree.resize(4*n,0);
-        buildSegmentTree(0,0,n-1,nums);
-
-
+    void add(int i,int diff){
+        for(;i<=N;i+=(i & (-i)))bit[i]+=diff;
         
     }
     
-    void update(int index, int val) {
-        int size=segmentTree.size()/4;
-        updateSegmentTree(0,0,size-1,index,val);
+    void update(int i, int val) {
+        int diff=val-nums[i];
+        nums[i]=val;
+        add(i+1,diff);
+        
+    }
+    int sum(int i){
+        int ans=0;
+        for(;i>0;i-=(i & (-i))){
+            ans+=bit[i];
+        }
+        return ans;
+
     }
     
     int sumRange(int left, int right) {
-        int n=segmentTree.size()/4;
-        return findSum(0,left,right,0,n-1);
+       
+        return sum(right+1)-sum(left);
+
         
     }
 };

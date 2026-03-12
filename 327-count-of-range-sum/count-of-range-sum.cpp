6599a1aed2b97b4cc8f02dcde81dcc4294389a0b
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2026-03-12 22:23:38 +0530

    Time: 1387 ms (7.52%) | Memory: 299.4 MB (24.13%) - LeetSync

--- 327-count-of-range-sum/count-of-range-sum.cpp
+++ 327-count-of-range-sum/count-of-range-sum.cpp
@@ -1,27 +1,16 @@
 #define ll long long
 class Solution {
 public:
-void update(int ind,int low,int high,int poss,int seg[]){
-    if(low==high){
-        seg[ind]++;
-        return;
-    }
-    int mid=(low+high)/2;
-    if(mid>=poss){
-        update(2*ind+1,low,mid,poss,seg);
-    }else{
-        update(2*ind+2,mid+1,high,poss,seg);
-    }
-    seg[ind]=seg[2*ind+1]+seg[2*ind+2];
+vector<int>bit;
+void update(int i){
+    for(;i<bit.size();i+=(i&(-i)))bit[i]++;
 }
-ll find(int ind,ll low,int high,ll l,ll r,int seg[]){
-    if(low>=l && high<=r)return seg[ind];
-    if(l>high || low>r)return 0;
-    int mid=(low+high)/2;
-   ll left=find(2*ind+1,low,mid,l,r,seg);
-    ll right=find(2*ind+2,mid+1,high,l,r,seg);
-    return left+right;
+ll search(int i){
+    ll ans=0;
+    for(;i>0;i-=(i&(-i)))ans+=bit[i];
+    return ans;
 }
+
     ll countRangeSum(vector<int>& nums, int lower, int upper) {
        set<ll>st;
        vector<ll>prefix(nums.size()+1,0);
@@ -40,19 +29,26 @@ ll find(int ind,ll low,int high,ll l,ll r,int seg[]){
        st.insert(prefix[i]-upper);
        st.insert(prefix[i]-lower);
        }
-       int i=0;
+       int i=1;
        for(auto it:st){
         mp[it]=i;
         i++;
        }
-       int seg[4*st.size()];
-       for(int i=0;i<4*st.size();i++)seg[i]=0;
-       update(0,0,st.size()-1,mp[prefix[0]],seg);
+       bit.resize(st.size()+2,0);
+       update(mp[0]);
+      
        for(int i=1;i<prefix.size();i++){
         int l=mp[prefix[i]-upper];
         int r=mp[prefix[i]-lower];
-        ans+=find(0,0,st.size()-1,l,r,seg);
-        update(0,0,st.size()-1,mp[prefix[i]],seg);
+        if(l==0){
+            ans+=search(r);
+        }else{
+             ans+=(search(r)-search(l-1));
+
+        }
+       
+        update(mp[prefix[i]]);
+
        }
        return ans;
 

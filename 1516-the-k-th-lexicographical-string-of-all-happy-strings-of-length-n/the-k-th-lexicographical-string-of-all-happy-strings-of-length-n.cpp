5c074b8e96860ce96569bdc596fbf0fe480f89ba
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2026-03-14 17:47:46 +0530

    Time: 51 ms (12.47%) | Memory: 27.7 MB (8.76%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-vector<string>result;
-void generate(int i,int n,int k,string ans){
-    if(n==ans.size()){
-        result.push_back(ans);
-
-        cout<<ans<<endl;
+vector<string>ans;
+void find(int n,string str){
+    if(str.size()==n){
+        ans.push_back(str);
         return;
     }
-
-    for(char ch='a';ch<='c';ch++){
-       if(i==0 || ch!=ans[i-1])
-       {
-        generate(i+1,n,k,ans+ch);
-
-       }
+    if(str.empty() || str[str.size()-1]!='a'){
+    str.push_back('a');
+    find(n,str);
+    str.pop_back();
     }
+     if(str.empty() || str[str.size()-1]!='b'){
+    str.push_back('b');
+    find(n,str);
+    str.pop_back();
+     }
+     if(str.empty() || str[str.size()-1]!='c'){
+    str.push_back('c');
+    find(n,str);
+    str.pop_back();
+     }
 }
     string getHappyString(int n, int k) {
-        generate(0,n,k,"");
-      
-       if(k>result.size()){
-        return "";
-       }
-       return result[k-1];
-        
-        
+       find(n,"");
+       sort(ans.begin(),ans.end());
+        if(ans.size()<k)return "";
+        return ans[k-1];
     }
 };

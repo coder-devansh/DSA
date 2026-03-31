8066bba08eeb518d4fe5335a0dca1507aaf907df
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2026-03-31 16:45:12 +0530

    Time: 39 ms (67.64%) | Memory: 28.2 MB (56.35%) - LeetSync

--- 741-cherry-pickup/cherry-pickup.cpp
+++ 741-cherry-pickup/cherry-pickup.cpp
@@ -3,6 +3,7 @@ class Solution {
 public:
 
 vector<vector<vector<int>>>dp;
+// memoization dp;
 ll find(int i1,int j1,int i2,vector<vector<int>>&grid){
     int j2=(i1+j1-i2);
     if(i1>=grid.size() || j1>=grid[0].size() || i2>=grid.size() || j2>=grid[0].size() || grid[i1][j1]==-1 || grid[i2][j2]==-1)return (int)-1e9;
@@ -21,10 +22,35 @@ ll find(int i1,int j1,int i2,vector<vector<int>>&grid){
 
 }
     int cherryPickup(vector<vector<int>>& grid) {
-        dp.resize(grid.size()+1,vector<vector<int>>(grid[0].size()+1,vector<int>(grid.size()+1,-1)));
+        int n=grid.size();
+        int m=grid[0].size();
+        dp.resize(grid.size()+1,vector<vector<int>>(grid[0].size()+1,vector<int>(grid.size()+1,-(int)1e9)));
+        dp[n-1][m-1][n-1]=(grid[n-1][m-1]);
         int ans= find(0,0,0,grid);
-        if(ans<0)return 0;
-        return ans;
+        for(int i1=n-1;i1>=0;i1--){
+            for(int j1=m-1;j1>=0;j1--){
+                for(int i2=n-1;i2>=0;i2--){
+                    int j2=(i1+j1-i2);
+           if(j2<0 || j2>=m)continue;
+           if(grid[i1][j1]==-1 || grid[i2][j2]==-1)continue;
+           if(i1 == n-1 && j1 == m-1) continue;
+            int cherry=grid[i1][j1];
+    if(i1!=i2){
+        cherry+=grid[i2][j2];
+    }
+                    int left=dp[i1+1][j1][i2+1];
+                    int right=dp[i1][j1+1][i2];
+                    int down=dp[i1+1][j1][i2];
+                    int up=dp[i1][j1+1][i2+1];
+                dp[i1][j1][i2]=max({left,right,down,up})+cherry;
+
+                }
+            }
+        }
+        return max(0,dp[0][0][0]);
+        // tabulation dp
+
+        
         
         
 

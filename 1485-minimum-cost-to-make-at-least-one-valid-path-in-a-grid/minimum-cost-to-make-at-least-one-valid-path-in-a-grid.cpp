858737821daca98d809b178056da4e6ac7d9dc3d
Devansh Bansal <159816253+coder-devansh@users.noreply.github.com>
2026-06-10 09:54:08 +0530

    Time: 28 ms (40.22%) | Memory: 18.9 MB (76.67%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,74 +1,50 @@
-struct pair_hash {
-    template <class T1, class T2>
-    size_t operator()(const pair<T1, T2>& pair) const {
-        return hash<T1>()(pair.first) ^ hash<T2>()(pair.second);
+struct compare{
+    bool operator()(pair<int,pair<int,int>>&a,pair<int,pair<int,int>>&b){
+        return a.first > b.first;
     }
 };
-
-
+int dir[5]={0,0,0,1,-1};
+int dic[5]={0,1,-1,0,0};
 class Solution {
 public:
-
     int minCost(vector<vector<int>>& grid) {
-        unordered_map<pair<int,int>,vector<pair<pair<int,int>,int>>,pair_hash>adj;
-        map<int,vector<int>>mp;
-        mp[1]={0,1};
-        mp[2]={0,-1};
-        mp[3]={1,0};
-        mp[4]={-1,0};
-        int dr[4]={-1,0,1,0};
-        int dc[4]={0,1,0,-1};
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
-                for(int k=0;k<4;k++){
-                    int dir=i+dr[k];
-                    int dic=j+dc[k];
-                    if(dir>=0 && dir<grid.size() && dic>=0 && dic<grid[0].size()){
-                      auto it=mp[grid[i][j]];
-                      if(it[0]==dr[k] && it[1]==dc[k]){
-                        adj[{i,j}].push_back({{dir,dic},0});
-                      }else{
-                        adj[{i,j}].push_back({{dir,dic},1});
-
-                      }
-                    }
-                }
-
-            }
-        }
         int n=grid.size();
         int m=grid[0].size();
-        int mini=INT_MAX;
-        vector<vector<int>>dist(grid.size(),vector<int>(grid[0].size(),INT_MAX));
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> q;
-      
-        q.push({0,{0,0}});
-        dist[0][0]=0;
-       
-        vector<vector<int>>vis(n+1,vector<int>(m+1,0));
-         vis[0][0]=1;
-        while(!q.empty()){
-           auto [ed,node]=q.top();
-           q.pop();
-           int x=node.first;
-           int y=node.second;
-           if(ed>dist[x][y])continue;
-           
-            
-            for(auto& [point,edw]:adj[node])
-            {
-               int nx=point.first;
-               int ny=point.second;
-               
-                if(dist[x][y]+edw<dist[nx][ny])
-                {
-                    dist[nx][ny]=dist[x][y]+edw;
-                    q.push({dist[nx][ny],{nx,ny}});
+       priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,compare>pq;
+        vector<vector<long long>>best(grid.size(),vector<long long>(grid[0].size(),INT_MAX));
+        pq.push({0,{0,0}});
+        best[0][0]=0;
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+            int cost=it.first;
+            int row=it.second.first;
+            int col=it.second.second;
+            if(row==n-1 && col==m-1)return cost;
+            if(best[row][col]<cost)continue;
+            int new_r=row+dir[grid[row][col]];
+            int new_c=col+dic[grid[row][col]];
+            if(new_r>=0 && new_c>=0 && new_r<n && new_c<m){
+                if(cost < best[new_r][new_c]){
+    best[new_r][new_c] = cost;
+    pq.push({cost,{new_r,new_c}});
+}
+            }
+            for(int k=1;k<=4;k++){
+                if(grid[row][col]!=k){
+                    int r=row+dir[k];
+                    int c=col+dic[k];
+                    if(r>=0 && c>=0 && r<n && c<m){
+                       if(cost + 1 < best[r][c]){
+    best[r][c] = cost + 1;
+    pq.push({best[r][c], {r,c}});
+}
+                    }
                 }
-
             }
         }
-        return dist[n-1][m-1];
+        return -1;
+
         
     }
 };
